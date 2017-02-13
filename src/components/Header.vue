abe4fae598ceeb816e48737ab4aaff6a01d21030
齋藤創 <z.me.3110.tabletennis@gmail.com>
2017-02-13 14:15:02 +0900

    HeaderのComponentの整形

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="Header">
-    <h1>{{ msg }}</h1>
+    <div id="title">
+      <h1>{{ msg }}</h1>
+    </div>
+    <div id="action">
+      <!-- TODO:商品追加処理の実装 -->
+      <button type="button" class="button">商品追加</button>
+    </div>
   </div>
 </template>
 
@@ -9,7 +15,7 @@ export default {
   name: 'Header',
   data () {
     return {
-      msg: 'ヘッダーの読み込みできた？'
+      msg: '買い物かごシステム'
     }
   }
 }
@@ -17,21 +23,60 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  .Header {
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 60px;
+    background-color: darkslateblue;
+    color: aliceblue;
+    text-align: center;
+  }
+  
+  #title{
+    float: left;
+    width: 70%;
+    height: 100%;
+  }
+  #action{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    float: right;
+    width: 30%;
+    height: 100%;
+  }
+  
+  .button {
+	display: inline-block;
+	outline: none;
+	cursor: pointer;
+	text-align: center;
+	text-decoration: none;
+	font: 14px/100% Arial, Helvetica, sans-serif;
+	padding: .5em 2em .55em;
+	text-shadow: 0 1px 1px rgba(0,0,0,.3);
+	-webkit-border-radius: .5em; 
+	-moz-border-radius: .5em;
+	border-radius: .5em;
+	-webkit-box-shadow: 0 1px 2px rgba(0,0,0,.2);
+	-moz-box-shadow: 0 1px 2px rgba(0,0,0,.2);
+	box-shadow: 0 1px 2px rgba(0,0,0,.2);
+    background-color: rgb(156, 230, 15);
+  }
+  .button:hover {
+  	text-decoration: none;
+  }
+  .button:active {
+  	position: relative;
+  	top: 1px;
+  }
+  
+  h1, h2 {
+    font-weight: normal;
+    margin: 0;
+    height: 60px;
+    line-height: 60px;
+  }
+  
 </style>
